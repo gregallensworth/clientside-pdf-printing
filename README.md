7fec08f7fba64b93e9641296738bb52e41f9c6b2
Gregor Allensworth <gregor@greeninfo.org>
2017-11-13 14:10:37 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# clientside-pdf-printing
---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+
+var element = document.getElementById('thing2');
+html2pdf(element, {
+  margin:       1,
+  filename:     'myfile.pdf',
+  image:        { type: 'jpeg', quality: 0.98 },
+  html2canvas:  { dpi: 192, letterRendering: true },
+  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+});
+
+
+});
