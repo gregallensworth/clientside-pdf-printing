d068a2591d76b8cf9a835358136fff9073fcf521
Gregor Allensworth <gregor@greeninfo.org>
2017-11-13 14:46:16 -0800

    success printing a thing that is not visible

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # clientside-pdf-printing
+
+Experiments in PDF generation purely client-side using JavaScript, Canvas, etc.
+
+At work, we're looking into ways to use less-expensive technologies for simpler sites. And one of the "small details" that ends up being a breaker, is PDF generation. Our current technique is WkHTMLtoPDF to generate very customized PDFs (statistics, charts, maps) of content not necessarily like that on the web page. This means a server so we can use command-line tools (e.g. pdfkit for Python).
+
+But if we can generate PDFs wholly client-side using static files, we could make strides in our quest to host at Github pages and other lower-cost and no-cost providers.
+
+
+## Goals for a Complete Success
+
+- [x] Export of an element of HTML
+  - [ ] Basic header, some content block and footer
+  - [ ] Images
+  - [ ] Hyperlinks
+  - [ ] Bootstrap responsive table
+  - [ ] Multi-page content
+- [x] PDF export of an element not visible on the page
+  - [ ] Some statistical readout, even if in HTML
+  - [ ] Alternative header simulating letterhead
+- [ ] PDF export of a Leaflet map
+  - [ ] Basic map with tiles
+  - [ ] Marker using SVG icon
+  - [ ] GeoJSON overlay
+- [ ] PDF export of charts
+  - [ ] D3
+  - [ ] Highcharts
+- [ ] Testing of page break behavior e.g. lengthy lists that would run off the bottom, so we know what workarounds or caveats are required
+
+
+## Credits
+
+This is simply tests and documentation of our usage of someone else's work, [https://github.com/eKoopmans/html2pdf the *html2pdf* system by eKoopmans] Donations and kudos should be directed to that author, and to those of the underlying libraries *html2canvas* and *jsPDF*.
+
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-$(document).ready(function () {
+// the PDF configuration
+// see also https://github.com/eKoopmans/html2pdf
+// see also https://html2canvas.hertzen.com/documentation.html#available-options
+// see also http://rawgit.com/MrRio/jsPDF/master/docs/jsPDF.html
+var PDF_EXPORT_CONFIG = {
+    filename: 'myfile.pdf',  // the file will be saved as this filename
+    jsPDF: { // jsPDF options such as page size
+        unit: 'in',
+        format: 'letter',
+        orientation: 'portrait'
+    },
+    margin: [ 0.5, 0.5, 0.5, 0.5 ], // page margin, in same units as above
+    enableLinks: true,  // make hyperlinks work in the PDF? yes!
+    image: { type: 'jpeg', quality: 1 },  // PDF image rendering settings; just keep these
+    html2canvas: { dpi: 192, letterRendering: true },  // / html2canvas options, usually fine as-given
+};
 
 
-var element = document.getElementById('thing2');
-html2pdf(element, {
-  margin:       1,
-  filename:     'myfile.pdf',
-  image:        { type: 'jpeg', quality: 0.98 },
-  html2canvas:  { dpi: 192, letterRendering: true },
-  jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-});
 
-
-});
+// PDF printing only works on things that are visible in the DOM
+// so, all DPF templates have a CSS class of .print_template and this has CSS making it non-visible
+// trick: remove that class, html2pdf() it, then re-add the class
+// it's so fast that the eye can't see it flicker onto the screen, but it does print!
+window.printPdf = function () {
+    var element = document.getElementById('thepdf');
+    $(element).removeClass('print_template');
+    html2pdf(element, PDF_EXPORT_CONFIG);
+    $(element).addClass('print_template');
+};
